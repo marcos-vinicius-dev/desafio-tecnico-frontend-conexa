58ff10a0d8d576f7dc672906bba7e1a1736bc93c
marcos vinicius <marcvinicius.1997@gmail.com>
2020-12-23 15:21:05 -0300

    feat: criando a estrutura do projeto

---
+++ src/mixins/axios/base.js
@@ -0,0 +1,35 @@
+import { slotScope } from '../../helpers'
+
+export default {
+
+  data () {
+    return {
+      data: undefined,
+      error: undefined,
+      loading: false,
+      response: undefined,
+      url: undefined,
+      variables: undefined
+    }
+  },
+
+  watch: {
+    loading (value) {
+      this.$emit('loading', value)
+    }
+  },
+
+  render (h) {
+    const props = {
+      data: this.data,
+      error: this.error,
+      loading: this.loading,
+      response: this.response,
+      submit: this.submit
+    }
+
+    const slotScoped = slotScope(this, 'default', props, this.$slots.default)
+
+    return this.tag ? h(this.tag, slotScoped) : h('div', slotScoped)
+  }
+}
---
+++ src/pages/Index.vue
@@ -0,0 +1,38 @@
+<script>
+import FormLogin from '../components/organisms/forms/FormLogin.vue'
+
+export default {
+  name: 'PageIndex',
+
+  components: {
+    FormLogin
+  },
+
+  data () {
+    return {
+      dataModel: {
+        email: '',
+        password: ''
+      }
+    }
+  }
+}
+</script>
+
+<template lang="pug">
+q-page(class="bg-grey-2 row justify-center items-center")
+  div(class="col-10 col-md-3 q-my-xl")
+    q-card(class="shadow-card")
+      div(class="row")
+        //- div(class="col-6 bg-primary")
+        //-   div(class="row justify-center items-center full-width full-height q-pa-xl")
+        //-     q-img(
+        //-       alt="Quasar logo"
+        //-       src="~assets/medicine.svg"
+        //-     )
+
+        //- div(class="col-6")
+        q-card-section
+          form-login
+
+</template>
