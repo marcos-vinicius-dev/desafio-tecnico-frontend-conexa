e82866d957f95d86521a39374f656b67b1f5092a
marcos vinicius <marcvinicius.1997@gmail.com>
2020-12-25 02:29:19 -0300

    feat: criando rotas

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,15 +1,57 @@
+const requireAuth = {
+  require: true,
+  redirectNotAllowed: '/'
+}
+
+const notRequireAuth = {
+  require: false,
+  redirectNotAllowed: '/dashboard'
+}
 
 const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      {
+        path: '',
+        meta: {
+          auth: notRequireAuth
+        },
+        component: () => import('pages/Index.vue')
+      },
+      {
+        path: 'consultationCreate',
+        meta: {
+          auth: requireAuth
+        },
+        component: () => import('pages/ConsultationCreate.vue')
+      },
+      {
+        name: 'consultationDetails',
+        path: 'consultationDetails/:consultation',
+        meta: {
+          auth: requireAuth
+        },
+        component: () => import('pages/ConsultationDetails.vue')
+      },
+      {
+        path: 'consultationList',
+        meta: {
+          auth: requireAuth
+        },
+        component: () => import('pages/ConsultationList.vue')
+      },
+      {
+        path: 'dashboard',
+        meta: {
+          auth: requireAuth
+        },
+        component: () => import('pages/Dashboard.vue')
+      }
     ]
   },
 
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '*',
     component: () => import('pages/Error404.vue')
