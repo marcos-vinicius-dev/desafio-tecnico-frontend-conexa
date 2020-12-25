f270fa4791361352a499a8e7f4923f6dd886bfd6
marcos vinicius <marcvinicius.1997@gmail.com>
2020-12-25 02:35:59 -0300

    feat: novas regras no lint para melhoria no codigo

--- .eslintrc.js
+++ .eslintrc.js
@@ -54,7 +54,6 @@ module.exports = {
     // allow paren-less arrow functions
     'arrow-parens': 'off',
     'one-var': 'off',
-    'quotes': 'off',
     'import/first': 'off',
     'import/named': 'error',
     'import/namespace': 'error',
@@ -65,6 +64,17 @@ module.exports = {
     'import/no-extraneous-dependencies': 'off',
     'prefer-promise-reject-errors': 'off',
 
+    'vue/attributes-order': 'error',
+    'vue/no-confusing-v-for-v-if': 'error',
+    'vue/no-v-html': 'error',
+    'vue/order-in-components': 'error',
+    'vue/this-in-template': 'error',
+    'vue/array-bracket-spacing': 'error',
+    'vue/arrow-spacing': 'error',
+    'vue/camelcase': 'error',
+    'vue/match-component-file-name': 'error',
+    'vue/no-boolean-default': 'error',
+    'vue/no-restricted-syntax': 'error',
 
     // allow debugger during development only
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
